8354a52f06fc4916f9564ed72f9564586600361b
Kashish Singh <kashishsingh281105@gmail.com>
2026-09-15 14:40:14 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int f(int n, vector<int>& dp) {
+        if (n == 1)
+            return 1;
+
+        if (n == 2)
+            return 2;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        dp[n] = f(n - 1, dp) + f(n - 2, dp);
+
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+
+        return f(n, dp);
+    }
+};
